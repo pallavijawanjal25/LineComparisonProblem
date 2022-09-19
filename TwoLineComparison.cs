5a05c9f4a033ef1a5577466804e14db8bc09e74f
Pallavi <109516737+pallavijawanjal25@users.noreply.github.com>
2022-09-19 22:12:42 +0530

    Update TwoLineComparison.cs

--- TwoLineComparison.cs
+++ TwoLineComparison.cs
@@ -1,50 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CompairLine_Problem
 {
-    public static class TwoLineComparison
+    public class LengthComparison
     {
-        public static void CheckTwoLineComparison(int l1, int m1, int l2, int m2)
+        public static void Comparison(double Length1,double Length2)
         {
-            int le1, le2;
-            if (l1 == l2)
+            if (Length1 < Length2)
             {
-                int comp = m1.CompareTo(m2);
-                if (comp == 0)
-                {
-                    Console.WriteLine("Both line are equals");
-                }
-                else if (comp >= 0)
-                {
-                    Console.WriteLine("Second line is Smaller than first");
-                }
-                else
-                {
-                    Console.WriteLine("Second line is bigger than First");
-                }
+                Console.WriteLine("Line1 is shorter than Line2");
             }
             else
             {
-                le1 = m1 - l1;
-                le2 = m2 - l2;
-                int comp = le1.CompareTo(le2);
-                if (comp == 0)
-                {
-                    Console.WriteLine("Both line are equals");
-                }
-                else if (comp >= 0)
-                {
-                    Console.WriteLine("Second line is Smaller than first");
-                }
-                else
-                {
-                    Console.WriteLine("Second line is bigger than First");
-                }
-                Console.ReadLine();
+                Console.WriteLine("Line2 is shorter than Line1");
             }
         }
     }
